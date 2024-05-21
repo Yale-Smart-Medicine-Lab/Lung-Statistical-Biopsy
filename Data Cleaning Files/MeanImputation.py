b2848a43211839d5c3e9e7406e4af247590ee211
Teresa Nguyen <teresa.nguyen@yale.edu>
2024-05-21 00:09:50 -0700

    Formatted Mean Imputation similarly to KNNImputation file

--- Data Cleaning Files/MeanImputation.py
+++ Data Cleaning Files/MeanImputation.py
@@ -1,25 +1,24 @@
 import pandas as pd
-import numpy as np  # Importing NumPy
+import numpy as np
 from sklearn.impute import SimpleImputer
 
-def impute_csv(file_path, output_file='NHIS_male_chosen_mean_imputed_data.csv'):
-    # Load data from a CSV file into a pandas DataFrame
-    data = pd.read_csv(file_path)
+fileNames = ['Data Files/male_filtered_70_NHIS', 'Data Files/female_filtered_70_NHIS.csv', 
+             'Data Files/female_filtered_30_NHIS.csv', 'Data Files/male_filtered_30_NHIS.csv', 
+             'Data Files/female_filtered_chosen_NHIS.csv', 'Data Files/male_filtered_chosen_NHIS.csv']
+suffix = '_mean_imputed'
+ext = '.csv'
 
-    # Create an imputer object with a mean imputation strategy
+for fileName in fileNames:
+    data = pd.read_csv(fileName + ext)
+    columns_to_keep = []
+    for col in data.columns.values:
+        # all the values for this feature are not null
+        if data[col].notnull().sum() != 0:
+            columns_to_keep.append(col)
+    
     imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-
-    # Fit the imputer on the data and transform it
-    # This step assumes that all columns of your data are numerical
-    imputed_data = imputer.fit_transform(data)
-
-    # Convert the array returned by SimpleImputer back into a pandas DataFrame
-    # This uses the original column names and indices
-    imputed_df = pd.DataFrame(imputed_data, columns=data.columns, index=data.index)
-
-    # Save the imputed data to a new CSV file
-    imputed_df.to_csv(output_file, index=False)
-    print(f"Imputed data saved to '{output_file}'.")
-
-# Example usage:
-impute_csv('Data Files/male_filtered_chosen_NHIS.csv')
+    imputed_data = imputer.fit_transform(data[columns_to_keep])
+    imputed_data = pd.DataFrame(imputed_data, columns=columns_to_keep)
+    
+    imputed_data.to_csv(fileName + suffix + ext, index=False)
+    print(f"Imputed data saved to '{fileName + suffix + ext}'.")
