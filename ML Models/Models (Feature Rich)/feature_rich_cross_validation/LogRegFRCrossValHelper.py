cde4506cb59072f07950b63ee6589bc05997baa5
Teresa Nguyen <teresa.nguyen@yale.edu>
2024-08-01 19:24:51 -0400

    Fix logistic regression

--- ML Models/Models (Feature Rich)/feature_rich_cross_validation/LogRegFRCrossValHelper.py
+++ ML Models/Models (Feature Rich)/feature_rich_cross_validation/LogRegFRCrossValHelper.py
@@ -12,7 +12,7 @@ female_ukb_path = 'Input/UKB_female_Lung_Imputed_MAIN.csv'
 (male_fpr_plco, male_tpr_plco, male_auc_plco), (male_fpr_ukb, male_tpr_ukb, male_auc_ukb), male_cv_means, male_cv_stds, male_plco_train_metrics, male_ukb_metrics, male_loss_curves = log_reg_cross_validation_and_train(male_plco_path, male_ukb_path, solver='lbfgs', max_iter=100)
 (female_fpr_plco, female_tpr_plco, female_auc_plco), (female_fpr_ukb, female_tpr_ukb, female_auc_ukb), female_cv_means, female_cv_stds, female_plco_train_metrics, female_ukb_metrics, female_loss_curves = log_reg_cross_validation_and_train(female_plco_path, female_ukb_path, solver='lbfgs', max_iter=100)
 
-log_reg_plot_loss_curves(male_loss_curves, female_loss_curves, 'Log Reg Feature Rich CV: Male and Female Loss Curves')
+plot_loss_curves(male_loss_curves, female_loss_curves, 'Log Reg Feature Rich CV: Male and Female Loss Curves')
 
 table_of_metrics('Log Reg Male Cross-Validation Metrics (Mean ± Std)', male_cv_means, male_cv_stds)
 table_of_metrics('Log Reg Male Training Metrics', None, None, male_plco_train_metrics)
